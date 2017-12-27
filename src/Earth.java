b73528b25455ad259d57d41d014adceb83daaa6c
hooke <hookes2@yahoo.com>
2017-12-27 10:19:44 -0700

    1. Created the first object from the human class. 2. Created a Constructor which is used to instantiate the Human class

--- src/Earth.java
+++ src/Earth.java
@@ -7,5 +7,13 @@ public class Earth {
         tom.heightInInches = 120;
         tom.age = 10;
         tom.speak();
+
+        Human joe = new Human();
+        joe.name = "Joe Smith";
+        joe.eyeColor = "Green";
+        joe.heightInInches = 200;
+        joe.age = 20;
+        joe.speak();
+
     }
 }
